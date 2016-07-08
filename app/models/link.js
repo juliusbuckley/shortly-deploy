c183812b6133079bc811debdffdb53dbb4a6ef83
Hack Reactor Students <accounts@hackreactor.com>
2016-07-07 17:35:31 -0700

    Begin refactoring models to Mongoose

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,46 @@
-var db = require('../config');
+var mongoose = require('../config');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+
+var linkSchema = new mongoose.Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number
 });
 
-module.exports = Link;
+linkSchema.pre('this', function() {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(model.get('url'));
+  this.set('code', shasum.digest('hex').slice(0, 5));
+});
+
+module.exports = mongoose.model('Link', linkSchema);
+
+
+
+
+
+
+
+
+
+
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function() {
+//     this.on('creating', function(model, attrs, options) {
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
